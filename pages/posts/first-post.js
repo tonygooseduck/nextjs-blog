83e444bb76637f95f22dece312d0089b0b3048ae
Charles Tang <tonygooseduck@gmail.com>
2022-03-11 16:31:49 +0800

    Added assets, css

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+	<Layout>
+	</Layout>
+  )
+}

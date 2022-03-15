800243c4c5ecd33f12d80b3a30c1dcefaf0b1999
Charles Tang <tonygooseduck@gmail.com>
2022-03-15 17:04:03 +0800

    First two blog posts on home page
    Create two posts in markdown
    
    Install gray matter
    
    Add fetch post data library
    
    Add blog post section on home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
 	<Layout home>
 	  <Head>
@@ -11,6 +12,29 @@ export default function Home() {
 	  <section className={utilStyles.headingMd}>
 		<p>Hello, my name is Iu Chian. I am a software engineer interested in building great applications.</p>
 	  </section>
+	  <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+		<h2 className={utilStyles.headingLg}>Blog</h2>
+		<ul className={utilStyles.list}>
+		  {allPostsData.map(({ id, date, title }) => (
+			<li className={utilStyles.listItem} key={id}>
+			  {title}
+			  <br />
+			  {id}
+			  <br />
+			  {date}
+			</li>
+		  ))}
+		</ul>
+	  </section>
 	</Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+	props: {
+	  allPostsData
+	}
+  }
 }

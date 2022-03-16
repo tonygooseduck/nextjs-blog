803bdf336567f9628afc4521fc54d4eb112138db
Charles Tang <tonygooseduck@gmail.com>
2022-03-16 22:29:38 +0800

    Polish post page and home page
    - Date component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
-import Layout from '../../components/layout'
 import Head from 'next/head'
-
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
@@ -9,12 +10,13 @@ export default function Post({ postData }) {
 	  <Head>
 		<title>{postData.title}</title>
 	  </Head>
-	  <br />
-	  {postData.id}
-	  <br />
-	  {postData.date}
-	  <br />
-	  <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+	  <article>
+		<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+		<div className={utilStyles.lightText}>
+		  <Date dateString={postData.date} />
+		</div>
+		<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+	  </article>
 	</Layout>
   )
 }

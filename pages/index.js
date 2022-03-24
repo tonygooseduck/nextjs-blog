3896d78ae03e596523789ea45d817cf426b07f8d
tonygooseduck <tonygooseduck@gmail.com>
2022-03-24 11:35:20 +0800

    Fix indentation (#2)
    * First try
    
    * Fix js file indentation
    
    * Retab index.js

--- pages/index.js
+++ pages/index.js
@@ -7,38 +7,38 @@ import { getSortedPostsData } from '../lib/posts'
 
 export default function Home({ allPostsData }) {
   return (
-	<Layout home>
-	  <Head>
-		<title>{siteTitle}</title>
-	  </Head>
-	  <section className={utilStyles.headingMd}>
-		<p>Hello, my name is Iu Chian. I am a software engineer with a non-traditional background. This is a blog that I would like to use as an engineering daybook. Basically just jotting down thoughts and ideas.</p>
-	  </section>
-	  <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-		<h2 className={utilStyles.headingLg}>Blog</h2>
-		<ul className={utilStyles.list}>
-		  {allPostsData.map(({ id, date, title }) => (
-			<li className={utilStyles.listItem} key={id}>
-			  <Link href={`/posts/${id}`}>
-				<a>{title}</a>
-			  </Link>
-			  <br />
-			  <small className={utilStyles.lightText}>
-				<Date dateString={date} />
-			  </small>
-			</li>
-		  ))}
-		</ul>
-	  </section>
-	</Layout>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, my name is Iu Chian. I am a software engineer with a non-traditional background. This is a blog that I would like to use as an engineering daybook. Basically just jotting down thoughts and ideas.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   )
 }
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
-	props: {
-	  allPostsData
-	}
+    props: {
+      allPostsData
+    }
   }
 }

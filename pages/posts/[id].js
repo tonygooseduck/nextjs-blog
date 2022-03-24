e3050faf8a3f5dce03ed8c1cc10865a6f876921c
Charles Tang <tonygooseduck@gmail.com>
2022-03-24 11:22:02 +0800

    Fix js file indentation

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,34 +6,34 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
   return (
-	<Layout>
-	  <Head>
-		<title>{postData.title}</title>
-	  </Head>
-	  <article>
-		<h1 className={utilStyles.headingXl}>{postData.title}</h1>
-		<div className={utilStyles.lightText}>
-		  <Date dateString={postData.date} />
-		</div>
-		<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-	  </article>
-	</Layout>
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
   )
 }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
-	paths,
-	fallback: false
+    paths,
+    fallback: false
   }
 }
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
   return {
-	props: {
-	  postData
-	}
+    props: {
+      postData
+    }
   }
 }
